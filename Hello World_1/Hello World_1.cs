cb08c85fc0fab6bdf5205ce36f5870a1c4327bb5
super <super@DESKTOP-5T05R8Q>
2023-12-05 14:20:37 +0100

    Add project files.

---
+++ Hello World_1/Hello World_1.cs
@@ -0,0 +1,23 @@
+namespace Hello_World_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			engine.GenerateInformation("Hello world !");
+		}
+	}
+}
